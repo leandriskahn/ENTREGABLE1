61ee33108040c9c88f11f804a9a5c61750c54a28
Archibaldo8 <leandrovegadure@gmail.com>
2022-08-11 23:43:49 -0300

    asadasd

---
+++ registro/js/validacion.js
@@ -0,0 +1,15 @@
+const nombre = document.getElementsById("nombre");
+const apellido = document.getElementsById("apellido");
+const pass1 = document.getElementById("password1")
+const pass2 = document.getElementById("password2");
+
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+document.addEventListener()
+
